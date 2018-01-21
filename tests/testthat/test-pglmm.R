ef3a08beada4a108f40812122146938473181cbf
daijiang <daijianglee@gmail.com>
2018-01-20 22:17:15 -0500

    update test file for pglmm

--- tests/testthat/test-pglmm.R
+++ tests/testthat/test-pglmm.R
@@ -8,16 +8,21 @@ dat = tidyr::gather(comm, key = "sp", value = "freq", -site) %>%
   left_join(traits, by = "sp")
 dat$pa = as.numeric(dat$freq > 0)
 
-formula = pa ~ 1 + shade + (1|sp__) + (1|site) + (1|site@sp)
-data = dat
-tree = phylotree
 
-test1 = phyr::communityPGLMM(freq ~ 1 + shade + (1|sp__) + (1|site) + (1|site@sp), dat, tree = phylotree, REML = F)
-test1r = phyr::communityPGLMM(freq ~ 1 + shade + (1|sp__) + (1|site) + (1|site@sp), dat, tree = phylotree, REML = F, cpp = F)
-test3 = phyr::communityPGLMM(pa ~ 1 + shade + (1|sp__) + (1|site) + (1|site@sp), dat, family = "binomial", tree = phylotree, REML = F)
-test3r = phyr::communityPGLMM(pa ~ 1 + shade + (1|sp__) + (1|site) + (1|site@sp), dat, family = "binomial", tree = phylotree, REML = F, cpp = F)
-expect_equivalent(test1, test1r)
-expect_equivalent(test3, test3r)
+test1_gaussian_cpp = phyr::communityPGLMM(freq ~ 1 + shade + (1|sp__) + (1|site) + (1|sp@site), 
+                             dat, tree = phylotree, REML = F, cpp = T, optimizer = "Nelder-Mead")
+test1_gaussian_r  = phyr::communityPGLMM(freq ~ 1 + shade + (1|sp__) + (1|site) + (1|sp@site), 
+                             dat, tree = phylotree, REML = F, cpp = F, optimizer = "Nelder-Mead")
+expect_equivalent(test1_gaussian_cpp, test1_gaussian_r)
+
+test2_binary_cpp = phyr::communityPGLMM(pa ~ 1 + shade + (1|sp__) + (1|site) + (1|sp@site), 
+                                        dat, family = "binomial", tree = phylotree, REML = F,
+                                        cpp = T, optimizer = "Nelder-Mead")
+test2_binary_r = phyr::communityPGLMM(pa ~ 1 + shade + (1|sp__) + (1|site) + (1|sp@site), 
+                                      dat, family = "binomial", tree = phylotree, REML = F, 
+                                      cpp = F, optimizer = "Nelder-Mead")
+expect_equivalent(test2_binary_cpp, test2_binary_r)
+
 # data prep for pez::communityPGLMM
 dat = arrange(dat, site, sp)
 nspp = n_distinct(dat$sp)
@@ -42,41 +47,27 @@ re.nested.rep <- list(1, sp = dat$sp, covar = solve(Vphy), site = dat$site)
 # can be named 
 re = list(re.sp = re.sp, re.sp.phy = re.sp.phy, re.nested.phy = re.nested.phy, re.site = re.site)
 
-microbenchmark::microbenchmark(
-  phyr::communityPGLMM(freq ~ 1 + shade + (1|sp__) + (1|site) + (1|site@sp), dat, tree = phylotree, REML = F),
-  phyr::communityPGLMM(freq ~ 1 + shade + (1|sp__) + (1|site) + (1|site@sp), dat, tree = phylotree, REML = F, cpp = F),
-  # pez::communityPGLMM(freq ~ 1 + shade, data = dat, sp = dat$sp, site = dat$site, random.effects = re, REML = F),
-  times = 5
-)
 
-microbenchmark::microbenchmark(
-  phyr::communityPGLMM(pa ~ 1 + shade + (1|sp__) + (1|site) + (1|site@sp), dat, 
-                       family = "binomial", tree = phylotree, REML = F, cpp = T),
-  phyr::communityPGLMM(pa ~ 1 + shade + (1|sp__) + (1|site) + (1|site@sp), dat, 
-                       family = "binomial", tree = phylotree, REML = F, cpp = F),
-  pez::communityPGLMM(pa ~ 1 + shade, data = dat, family = "binomial", sp = dat$sp, 
-                      site = dat$site, random.effects = re, REML = F),
-  times = 5
-)
 
-test2 <- pez::communityPGLMM(freq ~ 1 + shade, data = dat, sp = dat$sp, site = dat$site, random.effects = re, REML = F)
-test4 <- pez::communityPGLMM(pa ~ 1 + shade, data = dat, family = "binomial", sp = dat$sp, site = dat$site, random.effects = re, REML = F)
-
-# begin tests
-test_that("testing gaussian models", {
-  expect_equivalent(test1$B, test2$B)
-  expect_equivalent(test1$B.se, test2$B.se)
-  expect_equivalent(test1$B.pvalue, test2$B.pvalue)
-  expect_equivalent(test1$ss, test2$ss)
-  expect_equivalent(test1$AIC, test2$AIC)
+test1_gaussian_pez <- pez::communityPGLMM(freq ~ 1 + shade, data = dat, sp = dat$sp, 
+                                          site = dat$site, random.effects = re, REML = F)
+test_that("testing gaussian models with pez package", {
+  expect_equivalent(test1_gaussian_cpp$B, test1_gaussian_pez$B)
+  expect_equivalent(test1_gaussian_cpp$B.se, test1_gaussian_pez$B.se)
+  expect_equivalent(test1_gaussian_cpp$B.pvalue, test1_gaussian_pez$B.pvalue)
+  expect_equivalent(test1_gaussian_cpp$ss, test1_gaussian_pez$ss)
+  expect_equivalent(test1_gaussian_cpp$AIC, test1_gaussian_pez$AIC)
 })
 
-test_that("testing binomial models", {
-  expect_equivalent(test3$B, test4$B)
-  expect_equivalent(test3$B.se, test4$B.se)
-  expect_equivalent(test3$B.pvalue, test4$B.pvalue)
-  expect_equivalent(test3$ss, test4$ss)
-  expect_equivalent(test3$AIC, test4$AIC)
+test2_binary_pez <- pez::communityPGLMM(pa ~ 1 + shade, data = dat, family = "binomial", 
+                                        sp = dat$sp, site = dat$site, random.effects = re, REML = F)
+
+test_that("testing binomial models with pez package", {
+  expect_equivalent(test2_binary_cpp$B, test2_binary_pez$B)
+  expect_equivalent(test2_binary_cpp$B.se, test2_binary_pez$B.se)
+  expect_equivalent(test2_binary_cpp$B.pvalue, test2_binary_pez$B.pvalue)
+  expect_equivalent(test2_binary_cpp$ss, test2_binary_pez$ss)
+  expect_equivalent(test2_binary_cpp$AIC, test2_binary_pez$AIC)
 })
 
 # test NAs
@@ -85,8 +76,10 @@ dat.na$freq[dat.na$freq == 0] = NA
 dat.na.rm = dat.na[!is.na(dat.na$freq),]
 
 test_that("testing data with NA, gaussian models", {
-  z.na = phyr::communityPGLMM(freq ~ 1 + shade + (1|sp__) + (1|site) + (1|site@sp), dat.na, tree = phylotree, REML = F)
-  z.na.rm = phyr::communityPGLMM(freq ~ 1 + shade + (1|sp__) + (1|site) + (1|site@sp), dat.na.rm, tree = phylotree, REML = F)
+  z.na = phyr::communityPGLMM(freq ~ 1 + shade + (1|sp__) + (1|site) + (1|sp@site), 
+                              dat.na, tree = phylotree, REML = F)
+  z.na.rm = phyr::communityPGLMM(freq ~ 1 + shade + (1|sp__) + (1|site) + (1|sp@site), 
+                                 dat.na.rm, tree = phylotree, REML = F)
   # NOTE: freq = NA is DIFFERENT from freq = 0 !
   expect_equivalent(z.na$B, z.na.rm$B)
   expect_equivalent(z.na$B.se, z.na.rm$B.se)
@@ -100,9 +93,9 @@ dat.na$pa[ina] = NA
 dat.na.rm = dat.na[!is.na(dat.na$pa),]
 
 test_that("testing data with NA, binomial models", {
-  z2.na = phyr::communityPGLMM(pa ~ 1 + shade + (1|sp__) + (1|site) + (1|site@sp), dat.na,
+  z2.na = phyr::communityPGLMM(pa ~ 1 + shade + (1|sp__) + (1|site) + (1|sp@site), dat.na,
                               family = "binomial", tree = phylotree, REML = F)
-  z2.na.rm = phyr::communityPGLMM(pa ~ 1 + shade + (1|sp__) + (1|site) + (1|site@sp), dat.na.rm, 
+  z2.na.rm = phyr::communityPGLMM(pa ~ 1 + shade + (1|sp__) + (1|site) + (1|sp@site), dat.na.rm, 
                                  family = "binomial", tree = phylotree, REML = F)
   # NOTE: pa = NA is DIFFERENT from pa = 0 !
   expect_equivalent(z2.na$B, z2.na.rm$B)
