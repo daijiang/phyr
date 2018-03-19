660037915349a3c3386d0011c6dc18950850e93f
daijiang <daijianglee@gmail.com>
2018-03-19 00:21:47 -0400

    update plot.re

--- R/pglmm.R
+++ R/pglmm.R
@@ -1864,16 +1864,18 @@ communityPGLMM.bayes <- function(formula, data = list(), family = "gaussian",
 #' @param show.image whether to show the images of random effects
 #' @param show.sim.image whether to show the images of simulated site by sp matrix. This can be useful to 
 #' see how the phylogenetic information were included.
-#' @param add.tree whether to add a phylogeny at the top of the simulated site by sp matrix plot
+#' @param add.tree.sp whether to add a phylogeny of species at the top of the simulated site by sp matrix plot, default is TRUE
+#' @param add.tree.site whether to add a phylogeny of sites at the right of the simulated site by sp matrix plot, default is FALSE
 #' @param tree.panel.space the number of lines between the phylogeney and the matrix plot, if add.tree is TRUE
 #' @param title.space the number of lines between the title and the matrix plot, if add.tree is TRUE
 #' @param tree.size the height of the phylogeney to be plotted (number of lines), if add.tree is TRUE
-#' @return a hidden list, the final plot can be saved using \code{ggplot2::ggsave} as it is a grid object.
+#' @param ... additional arguments for \code{Matrix::image()} or \code{lattice::levelplot()}.
+#' @return a hidden list, including the covariance matrices and simulated site by species matrices. Individual plots are saved as \code{plt_re_list} and \code{plt_sim_list}. If \code{show.image} or \code{show.sim.image} is TRUE, the corresponding final plot (\code{plt_re_all_in_one} or \code{plt_sim_all_in_one}) can be saved as external file using \code{ggplot2::ggsave} as it is a grid object.
 #' @export
 communityPGLMM.plot.random.effects <- function(
   formula, data, family = "gaussian", tree = NULL, tree_site = NULL, repulsion = FALSE, 
-  show.image = NULL, show.sim.image = NULL, random.effects = NULL, add.tree = TRUE,
-  tree.panel.space = 0.5, title.space = 5, tree.size = 3) {
+  show.image = NULL, show.sim.image = NULL, random.effects = NULL, add.tree.sp = TRUE, add.tree.site = FALSE,
+  tree.panel.space = 0.5, title.space = 5, tree.size = 3, ...) {
   data$sp <- as.factor(data$sp)
   data$site <- as.factor(data$site)
   
@@ -1921,6 +1923,9 @@ communityPGLMM.plot.random.effects <- function(
     Y.mat$site = NULL
     names(Y.mat) = gsub(pattern = "^Y", replacement = "", names(Y.mat))
     sim[[i]] <- as(as.matrix(Y.mat), "denseMatrix")[, tree$tip.label]
+    if(!is.null(tree_site)){ # bipartite problems
+      sim[[i]] <- sim[[i]][tree_site$tip.label, ]
+    }
   }
   names(sim) <- names(random.effects)
   
@@ -1938,18 +1943,26 @@ communityPGLMM.plot.random.effects <- function(
   n_col <- ceiling(length(vcv)^0.5)
   n_row <- (length(vcv) - 1) %/% n_col + 1
   
-  if (show.image) {
-    pl = vector("list", length = nv)
+  pl = vector("list", length = nv)
+  if(nrow(data) <= 200){ # tick marks at edge of each site
     for (i in 1:nv) {
       pl[[i]] = image(vcv[[i]], main = names(vcv)[i], ylab = "", xlab = "", sub = "",
                       scales = list(x = list(at = nspp * (1:nsite)), 
-                                    y = list(at = nspp * (1:nsite))))
+                                    y = list(at = nspp * (1:nsite))), ...)
     }
+  } else { # even tick marks
+    for (i in 1:nv) {
+      pl[[i]] = image(vcv[[i]], main = names(vcv)[i], ylab = "", xlab = "", sub = "", ...)
+    }
+  }
+  
+  if (show.image) {
     do.call(gridExtra::grid.arrange, c(pl, ncol = n_col, nrow = n_row))
     pl_re_all = do.call(gridExtra::arrangeGrob, c(pl, ncol = n_col, nrow = n_row))
   }
  
-  if(add.tree){
+  if(add.tree.sp){
+    if(is.null(tree)) stop("tree not specified")
     if(!ape::is.ultrametric(tree)){
       # correct round offs, force to be ultrametric
       h <- diag(ape::vcv(tree))
@@ -1960,27 +1973,65 @@ communityPGLMM.plot.random.effects <- function(
     hc <- ape::as.hclust.phylo(tree)
   }
   
-  if (show.sim.image) {
-    pl_sim = vector("list", length = nv)
-    for (i in 1:nv) {
-      if(add.tree){
-        plx = image(sim[[i]], main = names(sim)[i], ylab = "Site", xlab = "Species", 
-                    sub = "", useAbs = FALSE, scales = list(tck = c(1,0)),
-                    legend = list(top = list(fun = latticeExtra::dendrogramGrob, 
-                                             args = list(x = as.dendrogram(hc), 
-                                                         side = "top", size = tree.size))))
-      } else {
-        plx = image(sim[[i]], main = names(sim)[i], ylab = "Site", 
-                    xlab = "Species", sub = "", useAbs = FALSE)
+  if(add.tree.site){
+    if(is.null(tree_site)) stop("tree_site not specified")
+      if(!ape::is.ultrametric(tree_site)){
+        # correct round offs, force to be ultrametric
+        h <- diag(ape::vcv(tree_site))
+        d <- max(h) - h
+        ii <- sapply(1:ape::Ntip(tree_site), function(x,y) which(y==x), y = tree_site$edge[,2])
+        tree_site$edge.length[ii] <- tree_site$edge.length[ii] + d
       }
-      pl_sim[[i]] = plx
+      hc_site <- ape::as.hclust.phylo(tree_site)
+  }
+  
+  pl_sim = vector("list", length = nv)
+  for (i in 1:nv) {
+    if(add.tree.sp & !add.tree.site){ # only add tree for sp
+      plx = image(sim[[i]], main = names(sim)[i], ylab = "Site", xlab = "Species", 
+                  sub = "", useAbs = FALSE, scales = list(tck = c(1,0)),
+                  legend = list(top = list(fun = latticeExtra::dendrogramGrob, 
+                                           args = list(x = as.dendrogram(hc), 
+                                                       side = "top", size = tree.size))), ...)
     }
     
-    if(add.tree){
+    if(!add.tree.sp & !add.tree.site){ # not to add trees 
+      plx = image(sim[[i]], main = names(sim)[i], ylab = "Site", 
+                  xlab = "Species", sub = "", useAbs = FALSE, ...)
+    }
+    
+    if(add.tree.site & !add.tree.sp){
+      plx = image(sim[[i]], main = names(sim)[i], ylab = "Site", xlab = "Species", 
+                  sub = "", useAbs = FALSE, scales = list(tck = c(1,0)), colorkey = FALSE,
+                  legend = list(right = list(fun = latticeExtra::dendrogramGrob, 
+                                             args = list(x = as.dendrogram(hc_site), 
+                                                         side = "right", size = tree.size))), ...)
+    }
+    
+    if(add.tree.sp & add.tree.site){
+      plx = image(sim[[i]], main = names(sim)[i], ylab = "Site", xlab = "Species", 
+                  sub = "", useAbs = FALSE, scales = list(tck = c(1,0)), colorkey = FALSE,
+                  legend = list(top = list(fun = latticeExtra::dendrogramGrob, 
+                                           args = list(x = as.dendrogram(hc), 
+                                                       side = "top", size = tree.size)),
+                                right = list(fun = latticeExtra::dendrogramGrob, 
+                                             args = list(x = as.dendrogram(hc_site), 
+                                                         side = "right", size = tree.size))), ...)
+    }
+    
+    pl_sim[[i]] = plx
+  }
+  
+  if (show.sim.image) {
+    if((add.tree.site | add.tree.sp) & length(random.effects) > 6){
+      message("Too many random terms to show, consider turn off phylogeny via \n `add.tree.sp = FALSE` and `add.tree.site = FALSE`")
+    }
+    
+    if(add.tree.sp & !add.tree.site){
       # plot to device
       do.call(gridExtra::grid.arrange, 
               c(lapply(pl_sim, update, 
-                       par.settings = list(layout.heights = list(key.top = tree.panel.space, 
+                       par.settings = list(layout.heights = list(key.top = tree.panel.space,
                                                                  main = title.space))), 
                 ncol = n_col, nrow = n_row))
       # output so can be saved later
@@ -1996,14 +2047,21 @@ communityPGLMM.plot.random.effects <- function(
   }
   
   if(show.image & show.sim.image){
-    return(invisible(list(vcv = vcv, sim = sim, tree = tree, plt_re = pl_re_all, plt_sim = pl_sim_all)))
+    return(invisible(list(vcv = vcv, sim = sim, tree = tree, 
+                          plt_re_all_in_one = pl_re_all, plt_sim_all_in_one = pl_sim_all,
+                          plt_re_list = pl, plt_sim_list = pl_sim)))
   }
   if(show.image){
-    return(invisible(list(vcv = vcv, sim = sim, tree = tree, plt_re = pl_re_all)))
+    return(invisible(list(vcv = vcv, sim = sim, tree = tree, 
+                          plt_re_list = pl, plt_sim_list = pl_sim,
+                          plt_re_all_in_one = pl_re_all)))
   }
   if(show.sim.image) {
-    return(invisible(list(vcv = vcv, sim = sim, tree = tree, plt_sim = pl_sim_all)))
+    return(invisible(list(vcv = vcv, sim = sim, tree = tree, 
+                          plt_re_list = pl, plt_sim_list = pl_sim,
+                          plt_sim_all_in_one = pl_sim_all)))
   }
-  return(invisible(list(vcv = vcv, sim = sim, tree = tree)))
+  return(invisible(list(vcv = vcv, sim = sim, tree = tree, 
+                        plt_re_list = pl, plt_sim_list = pl_sim)))
 }
 
