1e2d9ed36bff1d827a18f73086347cfca9fb17d1
Lucas Nell <lucas@lucasnell.com>
2018-03-23 16:05:19 -0500

    Changed "corphylo" to "cor_phylo" in cor_phylo_info.R

--- R/cor_phylo_info.R
+++ R/cor_phylo_info.R
@@ -7,15 +7,15 @@
 
 check_phy <- function(phy) {
   if (!inherits(phy, "phylo")) {
-    stop("\nIn the call to corphylo the input phylogeny is not of class \"phylo\".",
+    stop("\nIn the call to cor_phylo the input phylogeny is not of class \"phylo\".",
          call. = FALSE)
   }
   if (is.null(phy$edge.length)) {
-    stop("\nIn the call to corphylo the input phylogeny has no branch lengths.",
+    stop("\nIn the call to cor_phylo the input phylogeny has no branch lengths.",
          call. = FALSE)
   }
   if (is.null(phy$tip.label)) {
-    stop("\nIn the call to corphylo the input phylogeny has no tip labels.",
+    stop("\nIn the call to cor_phylo the input phylogeny has no tip labels.",
          call. = FALSE)
   }
   
@@ -34,7 +34,7 @@ extract_species <- function(species, data, phy) {
   
   
   if (!length(species) %in% c(1, n)) {
-    stop("\nThe species argument to corphylo must be a vector ",
+    stop("\nThe species argument to cor_phylo must be a vector ",
          "of length 1 or n, where n is the number of elements in the input dataset",
          call. = FALSE)
   }
@@ -44,15 +44,15 @@ extract_species <- function(species, data, phy) {
     spp_vec <- species
   }
   if (sum(duplicated(spp_vec)) > 0) {
-    stop("\nDuplicate species not allowed in corphylo.", call. = FALSE)
+    stop("\nDuplicate species not allowed in cor_phylo.", call. = FALSE)
   }
   if (!all(spp_vec %in% phy$tip.label)) {
-    stop("\nIn corphylo, the `species` argument has one or more species not ",
+    stop("\nIn cor_phylo, the `species` argument has one or more species not ",
          "found in the phylogeny. Please filter the input data and try again.",
          call. = FALSE)
   } else if (!all(phy$tip.label %in% spp_vec)) {
     cat(paste(spp_vec, collapse = ' '))
-    stop("\nIn corphylo, the phylogeny has one or more species not found ",
+    stop("\nIn cor_phylo, the phylogeny has one or more species not found ",
          "in the `species` argument. ",
          "Please filter the input phylogeny and try again.",
          call. = FALSE)
@@ -74,7 +74,7 @@ extract_matrices <- function(formula, data, phy, spp_vec) {
   n <- length(phy$tip.label)
   
   if (!inherits(formula, "formula") || length(formula) != 3L) {
-    stop("\nAll formulas in corphylo must be formula objects of one of the ",
+    stop("\nAll formulas in cor_phylo must be formula objects of one of the ",
          'following forms: "trait ~ 1", "trait ~ covariate(s)", ',
          'or "trait ~ covariate(s) | measurement error"',
          call. = FALSE)
@@ -89,7 +89,7 @@ extract_matrices <- function(formula, data, phy, spp_vec) {
       # Looking for any punctuation other than "_" or "." in the measurement
       # error part:
       if (grepl("(?!\\.)(?!_)[[:punct:]]", term_se[2], perl = TRUE)) {
-        stop("\nThe following formula input to corphylo has >1 variable after ", 
+        stop("\nThe following formula input to cor_phylo has >1 variable after ", 
              "the \"|\":",
              paste(paste(terms(formula))[c(2, 1, 3)], collapse = ' '),
              "\nMore than one estimate of measurement error is not allowed. ",
@@ -113,7 +113,7 @@ extract_matrices <- function(formula, data, phy, spp_vec) {
       formula <- as.formula(paste0(terms(formula)[[2]], "~", term_se[1]))
       # Make sure the SeM object is of proper length:
       if (length(SeM) != n) {
-        stop("\nIn the call to corphylo, the number of items in the ",
+        stop("\nIn the call to cor_phylo, the number of items in the ",
              "measurement error vector/column does not match the length of ",
              "the tree.",
              "This error is occurring for the variable ", paste(formula)[2], ".",
@@ -125,7 +125,7 @@ extract_matrices <- function(formula, data, phy, spp_vec) {
   X <- model.frame(formula, data)
   X <- as.matrix(X[, 1, drop = FALSE])
   if (nrow(X) != n) {
-    stop("\nIn the call to corphylo, the number of items in the ",
+    stop("\nIn the call to cor_phylo, the number of items in the ",
          "trait vector/column does not match the length of ",
          "the tree.",
          "This error is occurring for the variable ", paste(formula)[2], ".",
@@ -135,7 +135,7 @@ extract_matrices <- function(formula, data, phy, spp_vec) {
   U <- model.matrix(formula, data)[, -1, drop = FALSE]
   if (ncol(U) == 0) U <- matrix(0, nrow = n, ncol = 1)
   if (nrow(U) != n) {
-    stop("\nIn the call to corphylo, the number of rows in the ",
+    stop("\nIn the call to cor_phylo, the number of rows in the ",
          "covariate vector(s)/column(s) does not match the length of ",
          "the tree.",
          "This error is occurring for the variable ", paste(formula)[2], ".",
