226fa0dddd4707f84a15db06a396cc78f5bcac13
Lucas Nell <lucas@lucasnell.com>
2018-04-05 17:27:30 -0500

    Updated cor_phylo doc except for examples

--- R/cor_phylo.R
+++ R/cor_phylo.R
@@ -225,7 +225,7 @@ cp_get_row_names <- function(par_names) {
 
 
 
-#' Correlations among Multiple Traits with Phylogenetic Signal
+#' Correlations among multiple traits with phylogenetic signal
 #' 
 #' This function calculates Pearson correlation coefficients for multiple continuous
 #' traits that may have phylogenetic signal, allowing users to specify measurement
@@ -235,7 +235,7 @@ cp_get_row_names <- function(par_names) {
 #' estimation of phylogenetic signal in multiple traits while incorporating
 #' correlations among traits. It is also possible to include independent variables
 #' (covariates) for each trait to remove possible confounding effects.
-#' \code{corphylo()} returns the correlation matrix for trait values, estimates
+#' `cor_phylo` returns the correlation matrix for trait values, estimates
 #' of phylogenetic signal for each trait, and regression coefficients for
 #' independent variables affecting each trait.
 #' 
@@ -258,119 +258,86 @@ cp_get_row_names <- function(par_names) {
 #' \deqn{V[2,1] = C[2,1](d1,d2)}
 #' \deqn{V[2,2] = C[2,2](d2) + M2}
 #' 
-#' where \eqn{C[i,j](d1,d2)} are derived from \code{phy} under the assumption of joint 
-#' OU evolutionary processes for each trait (see Zheng et al. 2009).  This formulation 
+#' where \eqn{C[i,j](d1,d2)} are derived from `phy` under the assumption of joint 
+#' OU evolutionary processes for each trait (see Zheng et al. 2009). This formulation 
 #' extends in the obvious way to more than two traits.
 #' 
 #'
-#' @param formulas a list of \code{p} formulas (class \code{\link{formula}}), 
-#'     one formula for each trait of interest. Formulas should take one of the following
-#'     forms: 
-#'     \describe{
-#'         \item{\code{trait ~ 1}}{traits without covariates or measurement error}
-#'         \item{\code{trait ~ covariate_1 + ... + covariate_N}}{
-#'             traits with \code{N} covariates
-#'         }
-#'         \item{\code{trait ~ 1 | measurement error}}{
-#'             traits with measurement error indicated by standard errors in
-#'             the \code{measurement error} column/vector
-#'         }
-#'         \item{\code{trait ~ covariate_1 + ... + covariate_N | measurement error}}{
-#'             traits with both covariates and measurement error
-#'         }
+#' @param formulas a list of `p` formulas (class \code{\link{formula}}), 
+#'   one formula for each trait of interest. Formulas should take one of the following
+#'   forms: 
+#'   \describe{
+#'     \item{`trait ~ 1`}{traits without covariates or measurement error}
+#'     \item{`trait ~ covariate_1 + ... + covariate_N`}{
+#'       traits with `N` covariates
 #'     }
-#' @param species a character vector or object in \code{data} indicating the order
-#'     of species for all trait and covariate values.
-#' @param phy a phylo object giving the phylogenetic tree.  The rownames of \code{phy}
-#'     must be the same as \code{X}, or alternatively, the order of values in rows
-#'     must match those in \code{X}.
+#'     \item{`trait ~ 1 | measurement error`}{
+#'       traits with measurement error indicated by standard errors in
+#'       the `measurement error` column/vector
+#'     }
+#'     \item{`trait ~ covariate_1 + ... + covariate_N | measurement error`}{
+#'       traits with both covariates and measurement error
+#'     }
+#'   }
+#' @param species a character vector or object in `data` indicating the order
+#'   of species for all trait and covariate values.
+#' @param phy a `phylo` object giving the phylogenetic tree.
 #' @param data an optional data frame, list, or environment that contains the
-#'     variables in the model. By default, variables are taken from the environment
-#'     from which \code{corphylo} was called.
-#' @param REML whether REML or ML is used for model fitting. Defaults to \code{TRUE}.
-#' @param method in \code{optim()}, either Nelder-Mead simplex minimization or 
-#'     SANN (simulated annealing) minimization is used. If SANN is used, it is 
-#'     followed by Nelder-Mead minimization.
-#'     Defaults to \code{"Nelder-Mead"}.
-#' @param constrain.d if \code{constrain.d} is \code{TRUE}, the estimates of d are 
-#'     constrained to be between zero and 1. This can make estimation more stable and 
-#'     can be tried if convergence is problematic. This does not necessarily lead to 
-#'     loss of generality of the results, because before using \code{corphylo}, 
-#'     branch lengths of \code{phy} can be transformed so that the "starter" tree
-#'     has strong phylogenetic signal.
-#'     Defaults to \code{FALSE}.
+#'   variables in the model. By default, variables are taken from the environment
+#'   from which `cor_phylo` was called.
+#' @param REML whether REML (versus ML) should be used for model fitting.
+#'   Defaults to `TRUE`.
+#' @param method method of optimization using `nlopt`. Options include 
+#'   "neldermead", "sbplx", "bobyqa", "cobyla", "praxis". See
+#'   \url{https://nlopt.readthedocs.io/en/latest/NLopt_Algorithms/} for more 
+#'   information.
+#' @param constrain_d if `constrain_d` is `TRUE`, the estimates of `d` are 
+#'   constrained to be between zero and 1. This can make estimation more stable and 
+#'   can be tried if convergence is problematic. This does not necessarily lead to 
+#'   loss of generality of the results, because before using `cor_phylo`, 
+#'   branch lengths of `phy` can be transformed so that the "starter" tree
+#'   has strong phylogenetic signal.
+#'   Defaults to `FALSE`.
 #' @param reltol a control parameter dictating the relative tolerance for convergence 
-#'     in the optimization; see \code{optim()}.
-#'     Defaults to \code{1e-6}.
-#' @param maxit.NM a control parameter dictating the maximum number of iterations 
-#'     in the optimization with Nelder-Mead minimization; see \code{optim()}.
-#'     Defaults to \code{1000}.
-#' @param maxit.SA a control parameter dictating the maximum number of iterations 
-#'     in the optimization with SANN minimization; see \code{optim()}.
-#'     Defaults to \code{1000}.
-#' @param temp.SA a control parameter dictating the starting temperature in the 
-#'     optimization with SANN minimization; see \code{optim()}.
-#'     Defaults to \code{1}.
-#' @param tmax.SA a control parameter dictating the number of function evaluations 
-#'     at each temperature in the optimization with SANN minimization; see \code{optim()}.
-#'     Defaults to \code{1}.
-#' @param verbose if \code{TRUE}, the model \code{logLik} and running estimates of the
-#'     correlation coefficients and values of \code{d} are printed each iteration
-#'     during optimization.
-#'     Defaults to \code{FALSE}.
-#' @param boot Number of parametric bootstrap replicates. Defaults to 0.
-#' @param boot_out Function to retrieve necessary info from corphylo object for each
-#'     bootstrap replicate. If defining your own function for this, make sure that 
-#'     the output is either a single number or a matrix row.
-#'     Defaults to \code{NULL}, which retrieves the correlation(s) and the 
-#'     \code{d}-values for the OU process (i.e., the measures of phylogenetic signal).
-#' @param n_cores Number of cores to use for parametric bootstrapping. Defaults to 1.
+#'   in the optimization; see `optim()`.
+#'   Defaults to `1e-6`.
+#' @param max_iter a control parameter dictating the maximum number of iterations 
+#'   in the optimization. Defaults to \code{1000}.
+#' @param verbose if `TRUE`, the model `logLik` and running estimates of the
+#'   correlation coefficients and values of `d` are printed each iteration
+#'   during optimization. Defaults to `FALSE`.
+#' @param boot Number of parametric bootstrap replicates. Defaults to `0`.
+#' @param n_cores Number of cores to use for parametric bootstrapping.
+#'   This argument is ignored if OpenMP is not enabled. Defaults to `1`.
 #'
 #' @return
 #' 
-#' An object of class "corphylo".
+#' An object of class "cor_phylo".
 #' 
-#' \item{cor.matrix}{the p x p matrix of correlation coefficients.}
-#' \item{d}{values of d from the OU process for each trait.}
-#' \item{B}{estimates of the regression coefficients, including intercepts.
-#'     Coefficients are named according to the list \code{U}. For example, B1.2 is 
-#'     the coefficient corresponding to \code{U[[1]][, 2]}, and if column 2 in
-#'     \code{U[[1]]} is named "colname2", then the coefficient will be B1.colname2.
-#'     Intercepts have the form B1.0.}
-#' \item{B.se}{standard errors of the regression coefficients.}
-#' \item{B.cov}{covariance matrix for regression coefficients.}
-#' \item{B.zscore}{Z scores for the regression coefficients.}
-#' \item{B.pvalue}{tests for the regression coefficients being different from zero.}
-#' \item{logLik}{he log likelihood for either the restricted likelihood
-#'     (\code{REML = TRUE}) or the overall likelihood (\code{REML = FALSE}).}
+#' \item{corrs}{the `p` x `p` matrix of correlation coefficients.}
+#' \item{d}{values of `d` from the OU process for each trait.}
+#' \item{B}{a matrix of regression-coefficient estimates, SE, Z-scores, and P-values,
+#'   respectively. Rownames indicate which coefficient it refers to.}
+#' \item{B_cov}{covariance matrix for regression coefficients.}
+#' \item{logLik}{the log likelihood for either the restricted likelihood
+#'   (\code{REML = TRUE}) or the overall likelihood (\code{REML = FALSE}).}
 #' \item{AIC}{AIC for either the restricted likelihood (\code{REML = TRUE}) or the
-#'     overall likelihood (\code{REML = FALSE}).}
+#'   overall likelihood (\code{REML = FALSE}).}
 #' \item{BIC}{BIC for either the restricted likelihood (\code{REML = TRUE}) or the
-#'     overall likelihood (\code{REML = FALSE}).}
-#' \item{REML}{whether REML is used rather than ML (\code{TRUE} or \code{FALSE}).}
-#' \item{constrain.d}{whether or not values of d were constrained to be between 0 
-#'     and 1 (\code{TRUE} or \code{FALSE}).}
-#' \item{X}{matrix \code{X} exactly as it was input; this is used for parametric
-#'     bootstrapping.}
-#' \item{XX}{values of \code{X} in vectorized form, with each trait \code{X[, i]}
-#'     standardized to have mean zero and standard deviation one.}
-#' \item{U}{matrix \code{U} exactly as it was input; this is used for parametric
-#'     bootstrapping.}
-#' \item{UU}{design matrix with values in UU corresponding to \code{XX}; each variable
-#'     \code{U[[i]][, j]} is standardized to have mean zero and standard deviation one.}
-#' \item{MM}{vector of measurement standard errors corresponding to \code{XX}, with 
-#'     the standard errors suitably standardized.}
-#' \item{Vphy}{the phylogenetic covariance matrix computed from \code{phy} and 
-#'     standardized to have determinant equal to one.}
-#' \item{R}{covariance matrix of trait values relative to the standardized values of
-#'     \code{XX}.}
-#' \item{V}{overall estimated covariance matrix of residuals for \code{XX} including
-#'     trait correlations, phylogenetic signal, and measurement error variances.
-#'     This matrix can be used to simulate data for parametric bootstrapping.
-#'     See examples.}
-#' \item{C}{matrix \code{V} excluding measurement error variances.}
-#' \item{convcode}{the convergence code provided by \code{optim()}.}
-#' \item{niter}{number of iterations performed by \code{optim()}.}
+#'   overall likelihood (\code{REML = FALSE}).}
+#' \item{LL_obj}{External pointer to a C++ object with info to calculate the
+#'   log-likelihood.}
+#' \item{niter}{Number of iterations the optimizer used.}
+#' \item{convcode}{Conversion code for the optimizer, which is positive on success and
+#'   negative on failure. See also
+#'   \url{https://nlopt.readthedocs.io/en/latest/NLopt_Reference/#return-values}.
+#' \item{matrices}{}
+#' \item{constrain_d}{}
+#' \item{call}{the matched call.}
+#' \item{par_names}{parameter names.}
+#' \item{bootstrap}{external pointer to a C++ object storing the output from bootstrap
+#'   replicates. Use \code{\link{get_bootstrap}} to view the confidence intervals or
+#'   raw data.}
 #' 
 #' @export
 #'
@@ -387,11 +354,11 @@ cp_get_row_names <- function(par_names) {
 #' M <- matrix(c(0.2, 0.4), nrow = 10, ncol = 2)
 #' rownames(M) <- phy$tip.label
 #' 
-#' corphylo(X = X, M = M, U = U, phy = phy, method = "Nelder-Mead")
+#' cor_phylo(X = X, M = M, U = U, phy = phy, method = "Nelder-Mead")
 #' 
 #' \dontrun{
 #'     ## Simulation example for the correlation between two variables. The example 
-#'     ## compares the estimates of the correlation coefficients from corphylo when
+#'     ## compares the estimates of the correlation coefficients from cor_phylo when
 #'     ## measurement error is incorporated into the analyses with three other cases:
 #'     ## (i) when measurement error is excluded, (ii) when phylogenetic signal is
 #'     ## ignored (assuming a "star" phylogeny), and (iii) neither measurement error
@@ -463,9 +430,9 @@ cp_get_row_names <- function(par_names) {
 #'         colnames(U[[2]]) <- "V1"
 #'         X[,2] <- X[,2] + B2[1] * U[[2]][,1] - B2[1] * mean(U[[2]][,1])
 #'         
-#'         z <- corphylo(X = X, M = M, U = U, phy = phy, method = "Nelder-Mead")
-#'         z.noM <- corphylo(X = X, U = U, phy = phy, method = "Nelder-Mead")
-#'         z.noP <- corphylo(X = X, M = M, U = U, phy = star, method = "Nelder-Mead")
+#'         z <- cor_phylo(X = X, M = M, U = U, phy = phy, method = "Nelder-Mead")
+#'         z.noM <- cor_phylo(X = X, U = U, phy = phy, method = "Nelder-Mead")
+#'         z.noP <- cor_phylo(X = X, M = M, U = U, phy = star, method = "Nelder-Mead")
 #'         
 #'         cor.list[rep] <- z$cor.matrix[1, 2]
 #'         cor.noM.list[rep] <- z.noM$cor.matrix[1, 2]
@@ -537,7 +504,7 @@ cor_phylo <- function(formulas, species, phy,
                       method = c("neldermead", "sbplx", "bobyqa", "cobyla", "praxis"),
                       constrain_d = FALSE, 
                       reltol = 1e-6, max_iter = 1000, 
-                      tmax_SA = 1, verbose = FALSE,
+                      verbose = FALSE,
                       boot = 0, boot_out = NULL, n_cores = 1) {
   
   method <- match.arg(method)
@@ -591,7 +558,7 @@ cor_phylo <- function(formulas, species, phy,
   
   # Add bootstrapping to output
   if (boot > 0) {
-    # output$bootstrap <- boot_corphylo(output, boot, boot_out, n_cores)
+    # output$bootstrap <- boot_cor_phylo(output, boot, boot_out, n_cores)
   }
   
   return(output)
